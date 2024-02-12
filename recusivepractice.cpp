80ebf100a14994bd6f76145beaf720d4b5f3caf2
Ben <root@ObviouslyBenjispc>
2024-02-12 10:32:24 -0600

    wrote recursive function for cpp file

--- recusivepractice.cpp
+++ recusivepractice.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+// recursive function for linear search algrothim
+
+int linearSearch(int *arr, int size, int value) {
+    if (size == 0) {
+        return -1;
+    }
+
+    if (arr[size - 1] == value) {
+        return size -1;
+    }
+
+    return linearSearch(arr, size - 1, value); 
+}
+
+int main () {
+
+}
